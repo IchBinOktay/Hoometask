16e2a7b34ae17cfecf521adb3a261de3a09aeb3a
IchBinOktay <165468527+IchBinOktay@users.noreply.github.com>
2024-07-28 09:17:23 +0400

    Update doctor.cs

--- Task3/doctor.cs
+++ Task3/doctor.cs
@@ -1,8 +1,18 @@
 ﻿using System;
 namespace Task3
 {
-    public class Doctor
+   public class Doctor
     {
-        public string Name { get; set; }
+        public string Name { get;set;}
+        public string Surname { get;set;}
+        public int Age { get;set;}
+        public List<Appointment> Appointments { get; set; }
+
+        public Doctor(string name,string surname,int age) 
+        {
+            Name = name; Surname = surname; Age = age;
+            Appointments = new List<Appointment>();
+        }
+     
     }
 }
